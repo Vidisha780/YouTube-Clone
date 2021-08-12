8f792c1b2edb99ea479bdc6363ab56d1074ee25d
Vidisha780 <72399141+Vidisha780@users.noreply.github.com>
2021-08-12 20:26:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ fetch(video_http + new URLSearchParams({
     key: api_key,
     part: 'snippet',
     chart: 'mostPopular',
-    maxResults: 50,    
+    maxResults: 48,    
     regionCode: 'IN'
 }))
 .then(response => response.json())
